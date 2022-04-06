57b71c9c852ac800d92eff82bdf0d08f0a1cb0e8
woods-one <0820ricer@gmail.com>
2022-04-06 20:09:33 +0900

    追加: Playerクラスの追加

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// プレイヤーの動きの処理
+/// </summary>
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float playerSpeed;
+
+    [SerializeField]
+    private float playerSlowSpeed;
+
+    private float playerNowSpeed;
+
+    void FixedUpdate()
+    {
+        MoveSlowPlayer();
+        MovePlayer();
+    }
+
+
+    /// <summary>
+    /// 矢印キーでプレイヤーの上下移動を制御するメソッド
+    /// </summary>
+    void MovePlayer()
+    {
+        Vector3 playerPos = transform.position;
+
+        if (Input.GetAxisRaw("Vertical") > 0)
+        {
+            playerPos.y += playerNowSpeed * Time.deltaTime;
+        }
+        else if (Input.GetAxisRaw("Vertical") < 0)
+        {
+            playerPos.y -= playerNowSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            playerPos.x += playerNowSpeed * Time.deltaTime;
+        }
+        else if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            playerPos.x -= playerNowSpeed * Time.deltaTime;
+        }
+
+        transform.position = playerPos;
+    }
+
+    /// <summary>
+    /// 左シフトキーを押している時にプレイヤーのスピードを遅くするメソッド
+    /// </summary>
+    void MoveSlowPlayer()
+    {
+        playerNowSpeed = (Input.GetKey(KeyCode.LeftShift)) ? playerSlowSpeed : playerSpeed;
+    }
+}
