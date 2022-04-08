6004942e24a08b063b5d739063e4eedf30ca8c94
satwu-0 <satouajinomoti@gmail.com>
2022-04-08 21:30:21 +0900

    追加：前回のプッシュの際言われた箇所を修正しました。

--- Assets/Scripts/TitleButton.cs
+++ Assets/Scripts/TitleButton.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
+using UnityEngine.SceneManagement;
 
 /// <summary>
-/// タイトル画面におけるかんたんモードのボタンを管理するクラス
+/// タイトル画面におけるボタンの動きを管理するクラス
 /// </summary>
 public class TitleButton : MonoBehaviour
 {
-
-public void OnClicked() {
-    transform.DOScale(1.1f,0.5f).SetEase(Ease.OutElastic).OnComplete(() => transform.DOScale(1f,0.5f));
-}
+    /// <summary>
+    /// ボタンがクリックされたときボタンが一瞬拡大する
+    /// </summary>
+    public void OnClicked()
+    {
+        Sequence sequence = DOTween.Sequence();
+        transform.DOScale(1.1f,0.5f).SetEase(Ease.OutElastic).OnComplete(() => transform.DOScale(1f,0.5f)).OnComplete(() => SceneManager.LoadScene(""));    
+    }
     
 }
