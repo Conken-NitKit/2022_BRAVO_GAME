e81ec7f6c9598d6ebc4960b37a6304df2f2c57e0
Ryota Minami <ryoryota253170@gmail.com>
2022-04-08 22:04:52 +0900

    修正：前回のプッシュ時に挙げられた箇所を修正しました

--- Assets/Scripts/EnemyEasyMode.cs
+++ Assets/Scripts/EnemyEasyMode.cs
@@ -2,17 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
-public class EnemyEasyMode : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        this.transform.DOMove(new Vector2(0f,4f),3f);
-    }
 
-    // Update is called once per frame
-    void Update()
+/// <summary>
+/// 敵の動きの名前空間
+/// </summary>
+namespace EnemyMoveEasy
+{
+    /// <summary>
+    /// 簡単モードの敵のクラス
+    /// </summary>
+    public class EnemyEasyMode : MonoBehaviour
     {
-        
+        // Start is called before the first frame update
+        private void Start()
+        {
+            EntryEnemy();
+        }
+
+        /// <summary>
+        /// 敵の登場
+        /// </summary>
+        private void EntryEnemy()
+        {
+            this.transform.DOMove(new Vector2(0f,4f),3f);
+        }
     }
 }
