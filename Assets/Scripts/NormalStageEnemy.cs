3b2811d620b2b90200fd1e3c1f1753c92816019c
Teshima <takuto0418340@gmail.com>
2022-04-09 21:28:37 +0900

    修正:プルリクエストで指摘された点の変更

--- Assets/Scripts/NormalStageEnemy.cs
+++ Assets/Scripts/NormalStageEnemy.cs
@@ -3,29 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 
+///<summary>
+///普通難易度の最初の敵の動きの処理
+///</summary>
+
 public class NormalStageEnemy : MonoBehaviour
 {  
-    public GameObject EnemyBullet;
+    [SerializeField]
+    private GameObject EnemyBullet;
+
     private void Start()
     {
         MoveFirstNormalStageEnemy();
         InvokeRepeating(nameof(AppearBullet),0.7f,0.6f);
         Invoke(nameof(StageClear),30f);
     }
-    //敵の動きの処理
-    public void MoveFirstNormalStageEnemy()
+
+    ///<summary>
+    ///敵が左右に往復するメソッド
+    ///</summary>
+    void MoveFirstNormalStageEnemy()
     {
             this.transform.DOMove(new Vector3(-3.5f,3.5f,0f),1.1f).SetDelay(0.7f).SetLoops(-1,LoopType.Yoyo).SetEase(Ease.InOutSine);
         
     }
 
-    //弾を呼び出す処理
+    ///<summary>
+    ///弾を呼び出すメソッド
+    ///</summary>
     void AppearBullet()
     {
         Instantiate (EnemyBullet,transform.position,Quaternion.identity);
     }
 
-    //敵の動きを止める処理
+    ///<summary>
+    //敵の動きを止めるメソッド
+    ///</summary>
     void StageClear()
     {
         DOTween.KillAll();
