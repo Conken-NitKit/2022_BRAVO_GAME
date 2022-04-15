b480d340947f9139618685f7d95030c6da370b7e
satwu-0 <satouajinomoti@gmail.com>
2022-04-15 16:56:15 +0900

    追加：タイマーの表示。

--- Assets/Scripts/TimeManager.cs
+++ Assets/Scripts/TimeManager.cs
@@ -1,12 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
 
 public class TimeManager : MonoBehaviour
 {
     public float limitSeconds { get; private set; }
 
     private float timeCountWaitSeconds = 0.02f;
+
+    [SerializeField]
+    private Text timerText;
+
+    void Start()
+    {
+        StartCoroutine(CountDownTime());
+    }
 
     /// <summary>
     /// 制限時間を減らしていくメソッド
@@ -18,6 +28,9 @@ public class TimeManager : MonoBehaviour
         while (limitSeconds >= 0)
         {
             limitSeconds -= timeCountWaitSeconds;
+            timerText.text = limitSeconds.ToString("f2");
+            transform.DOPunchScale(new Vector2(0.1f,0.1f),timeCountWaitSeconds);
+            
             yield return new WaitForSeconds(timeCountWaitSeconds);
         }
     }
---
+++ Assets/Scripts/Time_TextAnimation.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+/// <summary>
+/// スコアのアニメーションを管理するクラス
+/// </summary>
+public class Time_TextAnimation : MonoBehaviour
+{
+    private void Start()
+    {
+        
+        transform.DOPunchScale(new Vector2(0.5f,0.5f),1f);    
+    }
+}
