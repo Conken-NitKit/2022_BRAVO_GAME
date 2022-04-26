679f5fb77af91670fa407809be13840519af47f1
woods-one <0820ricer@gmail.com>
2022-04-26 23:30:12 +0900

    修正: スコアが大きくなりすぎるバグを修正しました

--- Assets/Scripts/UI/ScoreText.cs
+++ Assets/Scripts/UI/ScoreText.cs
@@ -14,14 +14,19 @@ public class ScoreText : MonoBehaviour
 
     public Text scoreText;
 
+    Vector3 defaultScale;
+
     private void Start()
     {
-       SetScore(); 
+        defaultScale = this.transform.localScale;
+        SetScore(); 
     }
     
     public void SetScore()
     {
         scoreText.text = $"ぐれいず : {playerStatus.playerGraze}";
+        transform.DOKill();
+        transform.localScale = defaultScale;
         transform.DOPunchScale(new Vector2(0.1f, 0.1f), 0.02f);
     }
 }
